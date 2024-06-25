5f6122962a1cfb7e66823597ea2c278986f53a93
Fabio Marques <fabio.marquezzx@gmail.com>
2024-06-24 21:23:50 -0300

    feat: trocando backend

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "fiap-backend"
+    bucket         = "tfstates-fiap"
     key            = "fiap_pagamentos_ms/terraform.tfstate"
     region         = "us-east-1"
     
