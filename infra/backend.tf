13e4966f0a903ed57357de14bb3a7ef72c6c4dd2
Fabio Marques <46504380+Marques0x01@users.noreply.github.com>
2024-06-28 18:00:14 -0300

    Update backend.tf

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "backend-fiap-pos"
+    bucket         = "tfstates-fiap-lanches"
     key            = "fiap_pagamentos_ms/terraform.tfstate"
     region         = "us-east-1"
     
